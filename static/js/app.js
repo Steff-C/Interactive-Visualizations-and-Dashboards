70fd92262097cc6835b15108a7525dfbdf58d10f
Steff-C <stephanie.core@mortenson.com>
2021-05-02 00:04:21 -0500

    put in code to draw bar graph

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,43 @@ console.log("app.js is loaded");
 
 function DrawBargraph(sampleId) {
     console.log(`DrawBargraph(${sampleId})`);
+
+    d3.json("data/samples.json").then(data => {
+        console.log(data);  
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        console.log(resultArray);
+        var result = resultArray[0];
+        console.log(result)
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(), //  TBD
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(), //TBD
+            orientation: "h"
+        }
+
+        var barArray = [barData];
+        
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+        
+        Plotly.newPlot("bar", barArray, barLayout);
+        
+        // console.log(sample_values);
+        // console.log(otu_ids);
+        // console.log(otu_labels);
+
+    });
 }
 
 function DrawBubblechart(sampleId) {
@@ -16,10 +53,10 @@ function ShowMetadata(sampleId) {
 function optionChanged(newSampleId) {
     console.log(`User selected(${newSampleId})`);
 
+    // Draw the graphs and the metadata
     DrawBargraph(newSampleId);
     DrawBubblechart(newSampleId);
     ShowMetadata(newSampleId);
-
 }
 
 
@@ -42,6 +79,7 @@ function InitDashboard() {
 
             var id = sampleNames[0];
 
+              // Draw the graphs and the metadata
             DrawBargraph(id);
             DrawBubblechart(id);
             ShowMetadata(id);
