12a4c3caf72133780607ea73c85d2bf0654f6167
Steff-C <stephanie.core@mortenson.com>
2021-05-01 23:23:59 -0500

    added an event handler and stubs for the function

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,28 @@
 ///////////////// THE CODE IN THIS FILE COMES FROM OFFICE HOURS WITH DOM /////////////
 console.log("app.js is loaded");
 
+function DrawBargraph(sampleId) {
+    console.log(`DrawBargraph(${sampleId})`);
+}
+
+function DrawBubblechart(sampleId) {
+    console.log(`DrawBubblechart(${sampleId})`);
+} 
+
+function ShowMetadata(sampleId) {
+    console.log(`ShowMetadata(${sampleId})`);
+} 
+
+function optionChanged(newSampleId) {
+    console.log(`User selected(${newSampleId})`);
+
+    DrawBargraph(newSampleId);
+    DrawBubblechart(newSampleId);
+    ShowMetadata(newSampleId);
+
+}
+
+
 function InitDashboard() {
     console.log("InitDashboard()");
 
@@ -17,11 +39,20 @@ function InitDashboard() {
                 .text(sampleId)
                 .property("value", sampleId);
             });
+
+            var id = sampleNames[0];
+
+            DrawBargraph(id);
+            DrawBubblechart(id);
+            ShowMetadata(id);
+
+
     });
 
-
-
     // Update the Bar Graph
+
+
+
     // Update the Bubble Chart
     // Update the Demographic Information
 
